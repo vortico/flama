30e7adad0cf6155fdbb476b0798cf00628b9b5e5
songww <sww4718168@gmail.com>
2018-10-08 23:40:44 +0800

    Missing annotations added on endpoints module.

--- starlette_api/endpoints.py
+++ starlette_api/endpoints.py
@@ -1,28 +1,30 @@
 import asyncio
+import typing
 
 from starlette.endpoints import HTTPEndpoint as BaseHTTPEndpoint
 from starlette.requests import Request
+from starlette.responses import Response
 
 from starlette_api.injector import Injector
 
 
 class HTTPEndpoint(BaseHTTPEndpoint):
-    def __init__(self, *args, injector: Injector = None, **kwargs):
+    def __init__(self, *args: typing.Any, injector: Injector = None, **kwargs: typing.Any):
         super().__init__(*args, **kwargs)
         self._injector = injector
 
     @property
-    def injector(self):
+    def injector(self) -> Injector:
         if self._injector is None:
             raise AttributeError("Injector is not initialized")
 
         return self._injector
 
     @injector.setter
-    def injector(self, injector):
+    def injector(self, injector: Injector):
         self._injector = injector
 
-    async def dispatch(self, request: Request, **kwargs):
+    async def dispatch(self, request: Request, **kwargs: typing.Any) -> Response:
         handler_name = "get" if request.method == "HEAD" else request.method.lower()
         handler = getattr(self, handler_name, self.method_not_allowed)
 
