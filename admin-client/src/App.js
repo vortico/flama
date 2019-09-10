5fb71bf326f84454e0e9959addcb5f4de813a549
Mario Beltrán Alarcón <belco90@gmail.com>
2019-09-10 11:45:36 +0200

    First approach for fetching admin resources

--- admin-client/src/App.js
+++ admin-client/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Admin, Resource, ListGuesser } from 'react-admin';
+import jsonApiClient from 'ra-jsonapi-client';
 
-function App() {
-  return (
-    <h1>Welcome to Flama Admin Panel!</h1>
-  );
-}
+const jsonApiSettings = {
+  total: 'count',
+  updateMethod: 'PUT',
+};
+const dataProvider = jsonApiClient('http://localhost:8000/', jsonApiSettings);
+
+
+const App = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource name="puppy" list={ListGuesser} />
+  </Admin>
+);
 
 export default App;
