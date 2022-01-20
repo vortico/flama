36fb40b3523625230fd47b4fc07293cee91baac8
José Antonio Perdiguero López <perdy@perdy.io>
2022-01-20 03:48:50 +0100

    :sparkles: Improve package interface

--- flama/__init__.py
+++ flama/__init__.py
@@ -1,4 +1,7 @@
+from starlette.config import Config  # noqa
+
 from flama.applications import *  # noqa
+from flama.components import *  # noqa
 from flama.endpoints import *  # noqa
 from flama.modules import Module  # noqa
 from flama.routing import *  # noqa
