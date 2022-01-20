5a716cb52e6731c26923ec8e82495ca4322f7fa9
José Antonio Perdiguero López <perdy@perdy.io>
2022-01-20 02:13:10 +0100

    :sparkles: Integration with SQLAlchemy through DatabaseModule

---
+++ tests/test_database.py
@@ -0,0 +1,15 @@
+import pytest
+import sqlalchemy
+from sqlalchemy.ext.asyncio import AsyncEngine
+
+from flama import Flama
+
+
+class TestCaseDatabaseModule:
+    @pytest.fixture
+    def app(self):
+        return Flama(database="sqlite+aiosqlite:///test")
+
+    def test_init(self, app):
+        assert isinstance(app.database.engine, AsyncEngine)
+        assert isinstance(app.database.metadata, sqlalchemy.MetaData)
