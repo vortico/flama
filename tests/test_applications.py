c31f570edb5e0412993b69d64671d88bdb292edf
José Antonio Perdiguero López <perdy@perdy.io>
2022-01-24 13:23:02 +0100

    :construction_worker: Fixes some tests broken by updating requirements

--- tests/test_applications.py
+++ tests/test_applications.py
@@ -1,29 +1,39 @@
+import sys
 from unittest.mock import MagicMock, call, patch
 
+import pytest
 from starlette.testclient import TestClient
 
 from flama import Flama
-from flama.applications import Lifespan
 
 
 class TestCaseFlama:
-    def test_lifespan(self):
-        with patch("flama.applications.Lifespan", spec=Lifespan) as lifespan_mock:
+    @pytest.fixture
+    def lifespan(self):
+        return MagicMock()
+
+    @pytest.mark.skipif(
+        sys.version_info < (3, 8), reason="requires python3.8 or higher to use async mocks"
+    )  # PORT: Remove when stop supporting 3.7
+    def test_lifespan(self, lifespan):
+        with patch("flama.applications.Lifespan", new=lifespan):
             app = Flama()
 
             with TestClient(app):
                 ...
 
-            assert lifespan_mock.call_args_list == [call(app, None)]
-            assert lifespan_mock()().__aenter__.call_args_list == [call()]
-            assert lifespan_mock()().__aexit__.call_args_list == [call(None, None, None)]
+            assert lifespan.call_args_list == [call(app, None)]
+            assert lifespan()().__aenter__.call_args_list == [call()]
+            assert lifespan()().__aexit__.call_args_list == [call(None, None, None)]
 
-    def test_lifespan_chain(self):
-        lifespan_mock = MagicMock()
-        app = Flama(lifespan=lifespan_mock)
+    @pytest.mark.skipif(
+        sys.version_info < (3, 8), reason="requires python3.8 or higher to use async mocks"
+    )  # PORT: Remove when stop supporting 3.7
+    def test_lifespan_chain(self, lifespan):
+        app = Flama(lifespan=lifespan)
 
         with TestClient(app):
             ...
 
-        assert lifespan_mock.__aenter__.call_args_list == [call()]
-        assert lifespan_mock.__aexit__.call_args_list == [call(None, None, None)]
+        assert lifespan.__aenter__.call_args_list == [call()]
+        assert lifespan.__aexit__.call_args_list == [call(None, None, None)]
