37e1f3281c896b57bf923b8bb168fc4ca7ec3779
José Antonio Perdiguero López <perdy@perdy.io>
2022-01-26 14:09:43 +0100

    :recycle: Use abc for codecs base classes

--- flama/codecs/base.py
+++ flama/codecs/base.py
@@ -1,31 +1,34 @@
+import abc
 import typing
 
 from flama import http, websockets
 
 
-class Codec:
+class Codec(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
     async def decode(self, item: typing.Any, **options):
-        raise NotImplementedError()
+        ...
 
+    @abc.abstractmethod
     async def encode(self, item: typing.Any, **options):
-        raise NotImplementedError()
+        ...
 
 
 class HTTPCodec(Codec):
     media_type = None
 
     async def decode(self, request: http.Request, **options):
-        raise NotImplementedError()
+        ...
 
     async def encode(self, item: typing.Any, **options):
-        raise NotImplementedError()
+        ...
 
 
 class WebsocketsCodec(Codec):
     encoding = None
 
-    async def decode(self, item: websockets.Message, **options):
-        raise NotImplementedError()
+    async def decode(self, message: websockets.Message, **options):
+        ...
 
     async def encode(self, item: typing.Any, **options):
-        raise NotImplementedError()
+        ...
