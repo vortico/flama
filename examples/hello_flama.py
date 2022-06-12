19aa485dca2084c7567a6a2a84f444706a9b7fe7
Miguel A. Duran-Olivencia <migduroli@gmail.com>
2022-06-13 00:53:37 +0200

    :recycle: Completed hello_flama.py

--- examples/hello_flama.py
+++ examples/hello_flama.py
@@ -1,8 +1,31 @@
 from flama import Flama
 
-app = Flama()
+import uvicorn
+
+app = Flama(
+    title="Hello-🔥",
+    version="1.0",
+    description="My first API",
+)
 
 
 @app.route("/")
 def home():
+    """
+    tags:
+        - Salute
+    summary:
+        Returns a warming message.
+    description:
+        This is a more detailed description of the method itself.
+        Here we can give all the details required and they will appear
+        automatically in the auto-generated docs.
+    responses:
+        200:
+            description: Warming hello message!
+    """
     return {"message": "Hello 🔥"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
