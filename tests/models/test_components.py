1678d28eb441a47a62edacd3645013f378934ef4
José Antonio Perdiguero López <perdy@perdy.io>
2022-06-12 12:56:14 +0200

    :sparkles: ModelComponent with get_model_type

--- tests/models/test_components.py
+++ tests/models/test_components.py
@@ -3,22 +3,22 @@ import tensorflow as tf
 from pytest import param
 from sklearn.linear_model import LogisticRegression
 
-from flama.models.components import Model, ModelComponentBuilder
+from flama.models.components import ModelComponentBuilder, SKLearnModel, TensorFlowModel
 
 
 class TestCaseModelComponent:
     @pytest.mark.parametrize(
-        ("model_file", "model"),
+        ("model_file", "component_model_class", "serialized_model_class"),
         (
-            param("tests/models/tensorflow_model.flm", tf.keras.models.Sequential, id="tensorflow"),
-            param("tests/models/sklearn_model.flm", LogisticRegression, id="sklearn"),
+            param("models/tensorflow_model.flm", TensorFlowModel, tf.keras.models.Sequential, id="tensorflow"),
+            param("models/sklearn_model.flm", SKLearnModel, LogisticRegression, id="sklearn"),
         ),
     )
-    def test_build(self, model_file, model):
+    def test_build(self, model_file, component_model_class, serialized_model_class):
         with open(model_file, "rb") as f:
             component = ModelComponentBuilder.loads(f.read())
 
-        model_wrapper = component.model
-        model_instance = model_wrapper.model
-        assert isinstance(model_wrapper, Model)
-        assert isinstance(model_instance, model)
+        model = component.model
+        model_instance = model.model
+        assert isinstance(model, component_model_class)
+        assert isinstance(model_instance, serialized_model_class)
