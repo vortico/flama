6711e5736d101db25e5a100d0ef3a45e9495dd37
José Antonio Perdiguero López <perdy@perdy.io>
2022-08-25 14:33:44 +0200

    :sparkles: Allows Flama application to decide which schema lib to use

--- tests/schemas/test_setup.py
+++ tests/schemas/test_setup.py
@@ -4,23 +4,34 @@ import marshmallow
 import pytest
 import typesystem
 
-from flama import schemas
+from flama import Flama
 
 
 class TestCaseSetup:
     def test_setup_default(self):
-        schemas._module.setup()
+        Flama()
+
+        from flama import schemas
+
         assert schemas.lib == typesystem
 
     def test_setup_typesystem(self):
-        schemas._module.setup("typesystem")
+        Flama(schema_library="typesystem")
+
+        from flama import schemas
+
         assert schemas.lib == typesystem
 
     def test_setup_marshmallow(self):
-        schemas._module.setup("marshmallow")
+        Flama(schema_library="marshmallow")
+
+        from flama import schemas
+
         assert schemas.lib == marshmallow
 
     def test_setup_no_lib_installed(self):
+        from flama import schemas
+
         with patch("flama.schemas.Module.available", PropertyMock(return_value=iter(()))), pytest.raises(
             AssertionError,
             match="No schema library is installed. Install one of your preference following instructions from: "
