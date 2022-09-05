405aa68c7d5a09f3e0d0736704dc41d82a0fa764
José Antonio Perdiguero López <perdy@perdy.io>
2022-09-05 14:37:47 +0200

    :sparkles: PyTorch serialization

---
+++ flama/serialize/serializers/pytorch.py
@@ -0,0 +1,23 @@
+import codecs
+import io
+import typing
+
+from flama.serialize.base import Serializer
+
+try:
+    import torch
+except Exception:  # pragma: no cover
+    torch = None  # type: ignore
+
+
+class PyTorchSerializer(Serializer):
+    def dump(self, obj: typing.Any) -> bytes:
+        assert torch is not None, "`pytorch` must be installed to use PyTorchSerializer."
+        buffer = io.BytesIO()
+        torch.jit.save(torch.jit.script(obj), buffer)
+        buffer.seek(0)
+        return codecs.encode(buffer.read(), "base64")
+
+    def load(self, model: bytes) -> typing.Any:
+        assert torch is not None, "`pytorch` must be installed to use PyTorchSerializer."
+        return torch.jit.load(io.BytesIO(codecs.decode(model, "base64")))
