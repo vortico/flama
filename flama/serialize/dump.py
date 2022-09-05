58ff78942cf5da862614854898f434e0c2cad4a5
José Antonio Perdiguero López <perdy@perdy.io>
2022-09-05 16:10:58 +0200

    :sparkles: PyTorch Model Resource

--- flama/serialize/dump.py
+++ flama/serialize/dump.py
@@ -4,8 +4,21 @@ from flama.serialize.types import Model, ModelFormat
 
 
 def dumps(lib: typing.Union[str, ModelFormat], model: typing.Any) -> bytes:
+    """Serialize a ML model using Flama format to bytes string.
+
+    :param lib: The ML library used for building the model.
+    :param model: The ML model.
+    :return: Serialized model using Flama format.
+    """
     return Model(ModelFormat(lib), model).to_bytes()
 
 
 def dump(lib: typing.Union[str, ModelFormat], model: typing.Any, fs: typing.BinaryIO) -> None:
+    """Serialize a ML model using Flama format to bytes stream.
+
+    :param lib: The ML library used for building the model.
+    :param model: The ML model.
+    :param fs: Output bytes stream.
+    :return: Serialized model using Flama format.
+    """
     fs.write(dumps(lib, model))
--- flama/serialize/load.py
+++ flama/serialize/load.py
@@ -4,8 +4,18 @@ from flama.serialize.types import Model
 
 
 def loads(data: bytes) -> Model:
+    """Deserialize a ML model using Flama format from a bytes string.
+
+    :param data: The serialized model.
+    :return: ML model.
+    """
     return Model.from_bytes(data)
 
 
 def load(fs: typing.BinaryIO) -> Model:
+    """Deserialize a ML model using Flama format from a bytes stream.
+
+    :param fs: Input bytes stream containing the serialized model.
+    :return: ML model.
+    """
     return loads(fs.read())
