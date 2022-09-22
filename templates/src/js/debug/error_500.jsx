6699c04a109fabc42175f6f29d6417a739668e0b
José Antonio Perdiguero López <perdy@perdy.io>
2022-09-22 18:12:41 +0200

    :sparkles: Update pyproject.toml according to Poetry 1.2

--- templates/src/js/debug/error_500.jsx
+++ templates/src/js/debug/error_500.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import '../../css/main.css'
 
 function ServerErrorPage () {
-  return (<div>Internal Server Error</div>)
+  return (<div>Internal Server Error 3</div>)
 }
 
 createRoot(document.getElementById('app')).render(
