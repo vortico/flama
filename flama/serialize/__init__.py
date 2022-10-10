d2fea60f212461362cf025f10f82d9b8e8a821d9
José Antonio Perdiguero López <perdy@perdy.io>
2022-10-10 11:22:55 +0200

    :construction_worker: Fix some types

--- flama/serialize/__init__.py
+++ flama/serialize/__init__.py
@@ -2,4 +2,4 @@ from flama.serialize.dump import dump, dumps
 from flama.serialize.load import load, loads
 from flama.serialize.types import Model, ModelFormat
 
-__all__ = ["dump", "dumps", "load", "loads", "ModelFormat"]
+__all__ = ["dump", "dumps", "load", "loads", "Model", "ModelFormat"]
