726c46bc72394e9fc8cc97b187fec2406d2490b2
migduroli <migduroli@gmail.com>
2022-10-18 22:49:05 +0200

    :wrench: Add host, port, workers, and reload, to serve method

--- flama/cli/serve.py
+++ flama/cli/serve.py
@@ -4,14 +4,13 @@ import uvicorn
 from flama import Flama
 
 
-@click.command()
+@click.command(context_settings={"auto_envvar_prefix": "FLAMA"})
 @click.argument("model-path", envvar="FLAMA_MODEL_PATH")
-@click.option("--model-url", envvar="FLAMA_MODEL_URL", default="/", show_default=True, help="Route of the model")
-@click.option("--model-name", envvar="FLAMA_MODEL_NAME", default="model", show_default=True, help="Name of the model")
+@click.option("--model-url", envvar="MODEL_URL", default="/", show_default=True, help="Route of the model")
+@click.option("--model-name", envvar="MODEL_NAME", default="model", show_default=True, help="Name of the model")
+@click.option("--app-title", envvar="APP_TITLE", default="Flama", show_default=True, help="Name of the application")
 @click.option(
-    "--app-title", envvar="FLAMA_APP_TITLE", default="Flama", show_default=True, help="Name of the application")
-@click.option(
-    "--app-version", envvar="FLAMA_APP_VERSION", default="0.1.0", show_default=True, help="Version of the application"
+    "--app-version", envvar="APP_VERSION", default="0.1.0", show_default=True, help="Version of the application"
 )
 @click.option(
     "--app-description",
@@ -20,7 +19,49 @@ from flama import Flama
     show_default=True,
     help="Description of the application",
 )
-def serve(model_path: str, model_url: str, model_name: str, app_title: str, app_version: str, app_description: str):
+@click.option(
+    "--host",
+    type=str,
+    default="127.0.0.1",
+    envvar="HOST",
+    help="Bind socket to this host.",
+    show_default=True,
+)
+@click.option(
+    "--port",
+    type=int,
+    default=8000,
+    envvar="PORT",
+    help="Bind socket to this port.",
+    show_default=True,
+)
+@click.option(
+    "--dev",
+    envvar="DEV",
+    is_flag=True,
+    default=False,
+    show_default=True,
+    help="Development mode (enables auto-reload).",
+)
+@click.option(
+    "--workers",
+    default=None,
+    type=int,
+    help="Number of worker processes. Defaults to the $WEB_CONCURRENCY environment"
+    " variable if available, or 1. Not valid with --dev.",
+)
+def serve(
+    model_path: str,
+    model_url: str,
+    model_name: str,
+    app_title: str,
+    app_version: str,
+    app_description: str,
+    host: str,
+    port: int,
+    dev: bool,
+    workers: int,
+):
     """
     Serve the ML model file at <MODEL_PATH> within a Flama Application.
     """
@@ -28,4 +69,10 @@ def serve(model_path: str, model_url: str, model_name: str, app_title: str, app_
         **{k: v for k, v in {"title": app_title, "version": app_version, "description": app_description}.items() if v}
     )
     app.models.add_model(model_url, model=model_path, name=model_name)  # type: ignore[attr-defined]
-    uvicorn.run(app)
+    uvicorn.run(
+        app,
+        host=host,
+        port=port,
+        workers=workers,
+        reload=dev,
+    )
