70a4e24b1885086852936173633f59447e6408ea
José Antonio Perdiguero López <perdy@perdy.io>
2022-11-30 00:07:34 +0100

    :sparkles: Robust serialization for TF based on SavedModel

--- flama/serialize/serializers/tensorflow.py
+++ flama/serialize/serializers/tensorflow.py
@@ -1,13 +1,32 @@
 import codecs
-import pickle
+import io
+import tarfile
 import typing
+from tempfile import TemporaryDirectory
 
 from flama.serialize.base import Serializer
+
+try:
+    import tensorflow as tf
+except Exception:  # pragma: no cover
+    tf = None  # type: ignore[misc, assignment]
 
 
 class TensorFlowSerializer(Serializer):
     def dump(self, obj: typing.Any, **kwargs) -> bytes:
-        return codecs.encode(pickle.dumps(obj), "base64")
+        assert tf is not None, "`tensorflow` must be installed to use TensorFlowSerializer."
+        buffer = io.BytesIO()
+        with TemporaryDirectory() as saved_model_dir, tarfile.open(fileobj=buffer, mode="w") as model_tar:
+            tf.keras.models.save_model(obj, saved_model_dir)
+            model_tar.add(saved_model_dir, arcname="")
+            model_tar.list()
+        buffer.seek(0)
+        return codecs.encode(buffer.read(), "base64")
 
     def load(self, model: bytes, **kwargs) -> typing.Any:
-        return pickle.loads(codecs.decode(model, "base64"))
+        assert tf is not None, "`tensorflow` must be installed to use TensorFlowSerializer."
+        with TemporaryDirectory() as saved_model_dir, tarfile.open(
+            fileobj=io.BytesIO(codecs.decode(model, "base64")), mode="r:"
+        ) as model_tar:
+            model_tar.extractall(saved_model_dir)
+            return tf.keras.models.load_model(saved_model_dir)
