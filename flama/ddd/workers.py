e4066b2085a2fdf78b4cd2ca192a74f2657e4707
José Antonio Perdiguero López <perdy@perdy.io>
2023-10-22 23:56:02 +0200

    :construction: Multiple connection manager

--- flama/ddd/workers.py
+++ flama/ddd/workers.py
@@ -17,6 +17,12 @@ __all__ = ["WorkerType", "AbstractWorker", "SQLAlchemyWorker"]
 
 
 class WorkerType(abc.ABCMeta):
+    """Metaclass for workers.
+
+    It will gather all the repositories defined in the class as class attributes as a single dictionary under the name
+    `_repositories`.
+    """
+
     def __new__(mcs, name: str, bases: t.Tuple[type], namespace: t.Dict[str, t.Any]):
         namespace["_repositories"] = types.Repositories(
             {
@@ -30,38 +36,75 @@ class WorkerType(abc.ABCMeta):
 
 
 class AbstractWorker(abc.ABC):
+    """Abstract class for workers.
+
+    It will be used to define the workers for the application. A worker consists of a set of repositories that will be
+    used to interact with entities and a mechanism for isolate a single unit of work.
+    """
+
     _repositories: t.ClassVar[t.Dict[str, t.Type[AbstractRepository]]]
 
     def __init__(self, app: t.Optional["Flama"] = None):
+        """Initialize the worker.
+
+        It will receive the application instance as a parameter.
+
+        :param app: Application instance.
+        """
         self._app = app
 
     @property
     def app(self) -> "Flama":
+        """Application instance.
+
+        :return: Application instance.
+        """
         assert self._app, "Worker not initialized"
         return self._app
 
     @app.setter
-    def app(self, app: "Flama"):
+    def app(self, app: "Flama") -> None:
+        """Set the application instance.
+
+        :param app: Application instance.
+        """
         self._app = app
 
     @app.deleter
-    def app(self):
+    def app(self) -> None:
+        """Delete the application instance."""
         self._app = None
 
     @abc.abstractmethod
-    async def __aenter__(self) -> "AbstractWorker":
+    async def begin(self) -> None:
+        """Start a unit of work."""
         ...
 
     @abc.abstractmethod
-    async def __aexit__(self, exc_type, exc_val, exc_tb):
+    async def end(self, rollback: bool = False) -> None:
+        """End a unit of work.
+
+        :param rollback: If the unit of work should be rolled back.
+        """
         ...
+
+    async def __aenter__(self) -> "AbstractWorker":
+        """Start a unit of work."""
+        await self.begin()
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        """End a unit of work."""
+        await self.end(rollback=exc_type is not None)
 
     @abc.abstractmethod
     async def commit(self) -> None:
+        """Commit the unit of work."""
         ...
 
     @abc.abstractmethod
     async def rollback(self) -> None:
+        """Rollback the unit of work."""
         ...
 
 
@@ -72,38 +115,61 @@ class SQLAlchemyWorker(AbstractWorker, metaclass=WorkerType):
 
     @property
     def connection(self) -> "AsyncConnection":
+        """Connection to the database.
+
+        :return: Connection to the database.
+        :raises AttributeError: If the connection is not initialized.
+        """
         try:
             return self._connection
         except AttributeError:
             raise AttributeError("Connection not initialized")
 
-    async def begin(self):
+    async def begin_transaction(self) -> None:
+        """Open a connection and begin a transaction."""
+
         self._connection = await self.app.sqlalchemy.open_connection()
         self._transaction = await self.app.sqlalchemy.begin_transaction(self._connection)
 
-    async def close(self, rollback: bool = False):
+    async def end_transaction(self, *, rollback: bool = False) -> None:
+        """End a transaction and close the connection.
+
+        :param rollback: If the transaction should be rolled back.
+        """
         if hasattr(self, "_transaction"):
-            await self.app.sqlalchemy.end_transaction(self._transaction, rollback)
+            await self.app.sqlalchemy.end_transaction(self._transaction, rollback=rollback)
             del self._transaction
 
         if hasattr(self, "_connection"):
-            await self.app.sqlalchemy.close_connection(self._transaction)
+            await self.app.sqlalchemy.close_connection(self._connection)
             del self._connection
 
-    async def __aenter__(self):
-        await self.begin()
+    async def begin(self) -> None:
+        """Start a unit of work.
+
+        Initialize the connection, begin a transaction, and create the repositories.
+        """
+        await self.begin_transaction()
+
         for repository, repository_class in self._repositories.items():
             setattr(self, repository, repository_class(self.connection))
-        return self
 
-    async def __aexit__(self, exc_type, exc_val, exc_tb):
-        await self.close(rollback=exc_type is not None)
+    async def end(self, *, rollback: bool = False) -> None:
+        """End a unit of work.
+
+        Close the connection, commit or rollback the transaction, and delete the repositories.
+
+        :param rollback: If the unit of work should be rolled back.
+        """
+        await self.end_transaction(rollback=rollback)
 
         for repository in self._repositories.keys():
             delattr(self, repository)
 
     async def commit(self):
+        """Commit the unit of work."""
         await self.connection.commit()
 
     async def rollback(self):
+        """Rollback the unit of work."""
         await self.connection.rollback()
--- flama/sqlalchemy.py
+++ flama/sqlalchemy.py
@@ -21,27 +21,67 @@ __all__ = ["metadata", "SQLAlchemyModule"]
 
 
 class ConnectionManager(abc.ABC):
+    """Abstract class for connection managers.
+
+    It will be used to manage the connections and transactions.
+    """
+
     def __init__(self, engine: "AsyncEngine") -> None:
+        """Initialize the connection manager.
+
+        :param engine: SQLAlchemy engine.
+        """
         self._engine = engine
 
     async def open(self) -> "AsyncConnection":
+        """Open a new connection to the database.
+
+        :return: Database connection.
+        """
         ...
 
+    @abc.abstractmethod
     async def close(self, connection: "AsyncConnection") -> None:
+        """Close the connection to the database.
+
+        :param connection: Database connection.
+        """
         ...
 
+    @abc.abstractmethod
     async def begin(self, connection: "AsyncConnection") -> "AsyncTransaction":
+        """Begin a new transaction.
+
+        :param connection: Database connection to use for the transaction.
+        :return: Database transaction.
+        """
         ...
 
-    async def end(self, transaction: "AsyncTransaction", rollback: bool = False) -> None:
-        ...
+    @abc.abstractmethod
+    async def end(self, transaction: "AsyncTransaction", *, rollback: bool = False) -> None:
+        """End a transaction.
 
-    async def rollback(self, transaction: "AsyncTransaction") -> None:
+        :param transaction: Database transaction.
+        :param rollback: If the transaction should be rolled back.
+        """
         ...
 
 
 class SingleConnectionManager(ConnectionManager):
+    """Connection manager that uses a single connection and transaction.
+
+    A single connection is opened when requested, and subsequent requests will share this same connection. Once all
+    clients finishes with the connection, it will be closed.
+
+    The transaction is similar to the transaction, but it will be created at first request and subsequent requests will
+    generate a nested transaction.
+    """
+
     def __init__(self, engine: "AsyncEngine") -> None:
+        """Initialize the connection manager.
+
+        :param engine: SQLAlchemy engine.
+        """
         super().__init__(engine)
         self._connection: t.Optional["AsyncConnection"] = None
         self._transaction: t.Optional["AsyncTransaction"] = None
@@ -49,22 +89,38 @@ class SingleConnectionManager(ConnectionManager):
 
     @property
     def connection(self) -> "AsyncConnection":
+        """Connection to the database.
+
+        :return: Connection to the database.
+        :raises SQLAlchemyError: If the connection is not initialized.
+        """
         if not self._connection:
-            raise exceptions.ApplicationError("Connection not initialised")
+            raise exceptions.SQLAlchemyError("Connection not initialized")
 
         return self._connection
 
     @property
     def transaction(self) -> "AsyncTransaction":
+        """Transaction to the database.
+
+        :return: Transaction to the database.
+        :raises SQLAlchemyError: If the transaction is not initialized.
+        """
         if not self._transaction:
-            raise exceptions.ApplicationError("Connection not initialised")
+            raise exceptions.SQLAlchemyError("Connection not initialized")
 
         return self._transaction
 
     async def open(self) -> "AsyncConnection":
+        """Open a new connection to the database.
+
+        The first client will open a new connection, and subsequent clients will share this same connection.
+
+        :return: Database connection.
+        """
         try:
             connection = self.connection
-        except exceptions.ApplicationError:
+        except exceptions.SQLAlchemyError:
             self._connection = connection = self._engine.connect()
             await self._connection.__aenter__()
 
@@ -72,60 +128,171 @@ class SingleConnectionManager(ConnectionManager):
         return connection
 
     async def close(self, connection: "AsyncConnection") -> None:
+        """Close the connection to the database.
+
+        If this is the last client, the connection will be closed.
+
+        :param connection: Database connection.
+        :raises SQLAlchemyError: If the connection is a different connection from the one opened.
+        """
+        if connection != self.connection:
+            raise exceptions.SQLAlchemyError("Wrong connection")
+
         self._clients -= 1
 
         if self._clients == 0:
-            await self.connection.__aexit__(None, None, None)
+            await connection.__aexit__(None, None, None)
 
     async def begin(self, connection: "AsyncConnection") -> "AsyncTransaction":
+        """Begin a new transaction.
+
+        If no transaction is started, a new transaction will be created. If a transaction is already started, a nested
+        transaction will be created.
+
+        :return: Database transaction.
+        :raises SQLAlchemyError: If the connection is a different connection from the one opened.
+        """
+        if connection != self.connection:
+            raise exceptions.SQLAlchemyError("Wrong connection")
+
         try:
-            transaction = await self.connection.begin_nested()
-        except exceptions.ApplicationError:
-            self._transaction = transaction = self.connection.begin()
+            transaction = connection.begin_nested()
+        except exceptions.SQLAlchemyError:
+            self._transaction = transaction = connection.begin()
 
         await transaction
         return transaction
 
-    async def end(self, transaction: "AsyncTransaction", rollback: bool = False) -> None:
+    async def end(self, transaction: "AsyncTransaction", *, rollback: bool = False) -> None:
+        """End a transaction.
+
+        :param transaction: Database transaction.
+        :param rollback: If the transaction should be rolled back.
+        """
         if rollback:
-            await self.transaction.rollback()
+            await transaction.rollback()
         else:
-            await self.transaction.commit()
+            await transaction.commit()
 
 
 class MultipleConnectionManager(ConnectionManager):
+    """Connection manager that handlers several connections and transactions."""
+
+    def __init__(self, engine: "AsyncEngine") -> None:
+        """Initialize the connection manager.
+
+        This manager keeps track of the connections and transactions, and it will close the connections when requested.
+        If a connection is closed, all their transactions will be commited and finished too.
+
+        :param engine: SQLAlchemy engine.
+        """
+        super().__init__(engine)
+        self._connections: t.Set["AsyncConnection"] = set()
+        self._transactions: t.Dict["AsyncTransaction", "AsyncConnection"] = {}
+
     async def open(self) -> "AsyncConnection":
-        ...
+        """Open a new connection to the database.
+
+        :return: Database connection.
+        """
+        connection = self._engine.connect()
+        await connection.__aenter__()
+        self._connections.add(connection)
+        return connection
 
     async def close(self, connection: "AsyncConnection") -> None:
-        ...
+        """Close the connection to the database.
+
+        :param connection: Database connection.
+        :raises SQLAlchemyError: If the connection is not initialized.
+        """
+        if connection not in self._connections:
+            raise exceptions.SQLAlchemyError("Connection not initialized")
+
+        for transaction in [tx for tx, con in self._transactions.items() if con == connection]:
+            await self.end(transaction)
+
+        await connection.__aexit__(None, None, None)
 
     async def begin(self, connection: "AsyncConnection") -> "AsyncTransaction":
-        ...
+        """Begin a new transaction.
 
-    async def commit(self, transaction: "AsyncTransaction") -> None:
-        ...
+        :param connection: Database connection to use for the transaction.
+        :return: Database transaction.
+        :raises SQLAlchemyError: If the connection is not initialized.
+        """
+        if connection not in self._connections:
+            raise exceptions.SQLAlchemyError("Connection not initialized")
 
-    async def end(self, transaction: "AsyncTransaction", rollback: bool = False) -> None:
-        ...
+        transaction = await connection.begin()
+        self._transactions[transaction] = connection
+        return transaction
+
+    async def end(self, transaction: "AsyncTransaction", *, rollback: bool = False) -> None:
+        """End a transaction.
+
+        :param transaction: Database transaction.
+        :param rollback: If the transaction should be rolled back.
+        :raises SQLAlchemyError: If the transaction is not started.
+        """
+        if transaction not in self._transactions:
+            raise exceptions.SQLAlchemyError("Transaction not started")
+
+        if rollback:
+            await transaction.rollback()
+        else:
+            await transaction.commit()
+
+        del self._transactions[transaction]
 
 
 class SQLAlchemyModule(Module):
+    """SQLAlchemy module.
+
+    It will initialize the SQLAlchemy engine and connection manager. It will also provide an interface to handle
+    connections and transactions.
+
+    Referring to how connections and transactions are managed, it can work in two modes: single connection and multiple
+    connections.
+    * Multiple connections (default): It will open a new connection and transaction for each request. It will keep
+    track of the connections, and it will close it when requested. Multiple transactions using the same connection is
+    also supported.
+    * Single connection: It will open a single connection and transaction, and all requests will share this connection
+    and transaction. The connection will be closed when all clients finishes. It will create a single transaction and
+    new transactions requested will be nested from this one.
+    """
+
     name = "sqlalchemy"
 
     def __init__(
-        self,
-        database: t.Optional[str] = None,
-        single_connection: bool = False,
-        engine_args: t.Optional[t.Dict[str, t.Any]] = None,
+        self, database: str, single_connection: bool = False, engine_args: t.Optional[t.Dict[str, t.Any]] = None
     ):
+        """Initialize the SQLAlchemy module.
+
+        Referring to how connections and transactions are managed, it can work in two modes: single connection and
+        multiple connections.
+        * Multiple connections (default): It will open a new connection and transaction for each request. It will keep
+        track of the connections, and it will close it when requested. Multiple transactions using the same connection
+        is also supported.
+        * Single connection: It will open a single connection and transaction, and all requests will share this
+        connection and transaction. The connection will be closed when all clients finishes. It will create a single
+        transaction and new transactions requested will be nested from this one.
+
+        :param database: Database connection string.
+        :param single_connection: If the module should work in single connection mode.
+        :param engine_args: Arguments to pass to the SQLAlchemy engine.
+        :raises ApplicationError: If SQLAlchemy is not installed.
+        """
         if sqlalchemy is None:
             raise exceptions.ApplicationError("sqlalchemy[asyncio] must be installed to use SQLAlchemyModule")
+
+        if not database:
+            raise exceptions.ApplicationError("Database connection string must be provided")
 
         super().__init__()
 
         self.database = database
-        self._metadata: "MetaData" = metadata  # type: ignore[assignment]
+        self.metadata: "MetaData" = metadata  # type: ignore[assignment]
         self._engine: t.Optional["AsyncEngine"] = None
         self._engine_args = engine_args or {}
         self._connection_manager: t.Optional["ConnectionManager"] = None
@@ -135,37 +302,64 @@ class SQLAlchemyModule(Module):
 
     @property
     def engine(self) -> "AsyncEngine":
-        if not self._engine:
-            raise exceptions.ApplicationError("SQLAlchemyModule not initialised")
+        """SQLAlchemy engine.
+
+        :return: SQLAlchemy engine.
+        :raises ApplicationError: If SQLAlchemyModule is not initialized.
+        """
+        if self._engine is None:
+            raise exceptions.ApplicationError("SQLAlchemyModule not initialized")
         return self._engine
 
     @property
     def connection_manager(self) -> "ConnectionManager":
-        if not self._connection_manager:
-            raise exceptions.ApplicationError("SQLAlchemyModule not initialised")
+        """Connection manager.
+
+        :return: Connection manager.
+        :raises ApplicationError: If SQLAlchemyModule is not initialized.
+        """
+        if self._connection_manager is None:
+            raise exceptions.ApplicationError("SQLAlchemyModule not initialized")
         return self._connection_manager
 
-    @property
-    def metadata(self) -> "MetaData":
-        return self._metadata
-
     async def open_connection(self) -> "AsyncConnection":
+        """Open a new connection to the database.
+
+        :return: Database connection.
+        """
         return await self.connection_manager.open()
 
     async def close_connection(self, connection: "AsyncConnection") -> None:
+        """Close the connection to the database.
+
+        :param connection: Database connection.
+        """
         return await self.connection_manager.close(connection)
 
     async def begin_transaction(self, connection: "AsyncConnection") -> "AsyncTransaction":
+        """Begin a new transaction.
+
+        :param connection: Database connection to use for the transaction.
+        :return: Database transaction.
+        """
         return await self.connection_manager.begin(connection)
 
-    async def end_transaction(self, transaction: "AsyncTransaction", rollback: bool = False) -> None:
-        return await self.connection_manager.end(transaction, rollback)
+    async def end_transaction(self, transaction: "AsyncTransaction", *, rollback: bool = False) -> None:
+        """End a transaction.
+
+        :param transaction: Database transaction.
+        :param rollback: If the transaction should be rolled back.
+        :return: Database transaction.
+        """
+        return await self.connection_manager.end(transaction, rollback=rollback)
 
     async def on_startup(self):
-        if self.database:
-            self._engine = create_async_engine(self.database, **self._engine_args)
-            self._connection_manager = self._manager_cls(self._engine)
+        """Initialize the SQLAlchemy engine and connection manager."""
+        self._engine = create_async_engine(self.database, **self._engine_args)
+        self._connection_manager = self._manager_cls(self._engine)
 
     async def on_shutdown(self):
-        if self.engine:
-            await self.engine.dispose()
+        """Close the SQLAlchemy engine and connection manager."""
+        await self.engine.dispose()
+        self._engine = None
+        self._connection_manager = None
