fbe9837fbbd522a6c0b961503c17cb5d84709fa8
José Antonio Perdiguero López <perdy@perdy.io>
2024-11-24 14:18:53 +0100

    :recycle: Types for endpoints

--- flama/types/__init__.py
+++ flama/types/__init__.py
@@ -1,5 +1,6 @@
 from flama.types.applications import *  # noqa
 from flama.types.asgi import *  # noqa
+from flama.types.endpoints import *  # noqa
 from flama.types.http import *  # noqa
 from flama.types.json import *  # noqa
 from flama.types.websockets import *  # noqa
