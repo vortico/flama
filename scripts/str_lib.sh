bc263395060de4f81578c32e323053d6aa40f8be
José Antonio Perdiguero López <perdy@perdy.io>
2025-02-14 21:26:45 +0100

    :sparkles: Replace poetry with uv (#175)

--- scripts/str_lib.sh
+++ scripts/str_lib.sh
@@ -2,5 +2,15 @@ NO_FORMAT="\033[0m"
 F_ITALIC="\e[3m"
 F_BOLD="\033[1m"
 C_RED1="\033[38;5;196m"
-C_YELLOW1="\033[38;5;226m"
 C_SPRINGGREEN2="\033[38;5;47m"
+C_YELLOW1="\033[38;5;226m"
+
+message() {
+  case $1 in
+  info) printf "🔥 %b\n" "$2" ;;
+  success) printf "✅ ${C_SPRINGGREEN2}%b${NO_FORMAT}\n" "$2" ;;
+  warning) printf "⚠️ ${F_ITALIC}${C_YELLOW1}%b${NO_FORMAT}\n" "$2" >&2 ;;
+  error) printf "🚨 ${F_BOLD}${C_RED1}%b${NO_FORMAT}\n" "$2" >&2 ;;
+  *) printf "%b\n" "$1" ;;
+  esac
+}
