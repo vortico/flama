21caff0bf22a975112a264c288585b6c7b595880
José Antonio Perdiguero López <perdy@perdy.io>
2025-02-19 14:04:58 +0100

    :label: Simplify pagination types

--- flama/resources/routing.py
+++ flama/resources/routing.py
@@ -1,14 +1,13 @@
 import inspect
 import typing as t
 
-from flama import exceptions
+from flama import exceptions, types
 from flama.pagination import paginator
 from flama.resources import data_structures
 from flama.routing import Mount, Route
 
 if t.TYPE_CHECKING:
     from flama import Flama
-    from flama.pagination.types import PaginationType
     from flama.resources import Resource
 
 __all__ = ["ResourceRoute", "resource_method"]
@@ -65,7 +64,7 @@ def resource_method(
     methods: t.Optional[t.Sequence[str]] = None,
     name: t.Optional[str] = None,
     *,
-    pagination: t.Optional[t.Union[str, "PaginationType"]] = None,
+    pagination: t.Optional[types.Pagination] = None,
     tags: t.Optional[dict[str, t.Any]] = None,
 ) -> t.Callable:
     """Decorator for adding useful info needed for generating resource routes.
--- flama/types/__init__.py
+++ flama/types/__init__.py
@@ -3,4 +3,5 @@ from flama.types.asgi import *  # noqa
 from flama.types.endpoints import *  # noqa
 from flama.types.http import *  # noqa
 from flama.types.json import *  # noqa
+from flama.types.pagination import *  # noqa
 from flama.types.websockets import *  # noqa
